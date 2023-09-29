ffb4214a81a0d02287b95e813f2eb3c047b307c4
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-09-29 07:17:04 +0530

    added scanner for user input

--- src/com/dayam/Main.java
+++ src/com/dayam/Main.java
@@ -1,7 +1,11 @@
 package com.dayam;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dayam");
+        System.out.println("Enter your thoughts");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
