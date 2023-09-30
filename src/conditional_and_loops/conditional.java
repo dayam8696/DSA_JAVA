1c797884a61b21d9a22029571cf3f2e148edcf1a
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-09-30 11:28:55 +0530

    added conditional

--- src/conditional_and_loops/conditional.java
+++ src/conditional_and_loops/conditional.java
@@ -1,4 +1,34 @@
 package conditional_and_loops;
 
 public class conditional {
+    public static void main(String[] args) {
+        /*
+            Syntax of if statements:
+            if (boolean expression T or F) {
+                // body
+            } else {
+                // do this
+            }
+        */
+        int salary = 24000;
+        if(salary>10000){
+            salary = salary+2000;
+        }
+        else{
+            salary = salary+1000;
+        }
+        System.out.println(salary);
+
+        // multiple if-else
+
+        if (salary > 10000) {
+            salary += 2000; // salary = salary + 2000
+        } else if (salary > 20000) {
+            salary += 3000;
+        } else {
+            salary += 1000;
+        }
+
+        System.out.println(salary);
+    }
 }
