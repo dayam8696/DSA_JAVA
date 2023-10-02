9cb2d2b997972977e3292caacbb82d260811ee29
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-02 07:27:27 +0530

    days questions

--- src/switch_case/days_in_week.java
+++ src/switch_case/days_in_week.java
@@ -1,7 +1,37 @@
 package switch_case;
 
+import java.util.Scanner;
+
 public class days_in_week {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the day number between 1-7");
+        int day = in.nextInt();
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wenessday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Please enter the valid day number ");
+                break;
+        }
     }
 }
