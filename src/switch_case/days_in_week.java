2a18daf5a79d60aabbf0f7600e0034e6c863aa4d
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-02 07:19:35 +0530

    sc update

---
+++ src/switch_case/days_in_week.java
@@ -0,0 +1,7 @@
+package switch_case;
+
+public class days_in_week {
+    public static void main(String[] args) {
+
+    }
+}
--- src/switch_case/fruit_question.java
+++ src/switch_case/fruit_question.java
@@ -7,21 +7,12 @@ public class fruit_question {
         Scanner in = new Scanner(System.in);
         System.out.println("enter the fruit name");
         String fruit = in.next();
-        switch (fruit){
-            case "apple":
-                System.out.println("sweet red fruit");
-                break;
-            case"mango":
-                System.out.println("King of fruits");
-                break;
-                case"orange":
-                    System.out.println("Round Fruit");
-                    break;
-            case "Graps":
-                System.out.println("Green Fruits");
-                break;
-            default:
-                System.out.println("please enter the valid fruit name ");
+        switch (fruit) {
+            case "apple" -> System.out.println("sweet red fruit");
+            case "mango" -> System.out.println("King of fruits");
+            case "orange" -> System.out.println("Round Fruit");
+            case "Graps" -> System.out.println("Green Fruits");
+            default -> System.out.println("please enter the valid fruit name ");
         }
     }
 }
