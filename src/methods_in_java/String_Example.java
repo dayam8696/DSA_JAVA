5e0995dbcc17e8c8f2713963835064a9d041e369
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-03 07:22:28 +0530

    updated method

---
+++ src/methods_in_java/String_Example.java
@@ -0,0 +1,13 @@
+package methods_in_java;
+
+public class String_Example {
+    public static void main(String[] args) {
+        String msg = greet();
+        System.out.println(msg);
+    }
+
+    static String greet(){
+        String greeting = "how are you ";
+        return greeting;
+    }
+}
--- src/methods_in_java/Sum.java
+++ src/methods_in_java/Sum.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+       int ans =  sum2();
+        System.out.println(ans);
 
     }
     static void sum (){
@@ -17,4 +18,15 @@ public class Sum {
         sum = num1+num2;
         System.out.println("The sum is :" + sum);
     }
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        int num1 , num2 , sum;
+        System.out.println("Enter the first number ");
+        num1 = in.nextInt();
+        System.out.println("Enter the second number ");
+        num2 = in.nextInt();
+        sum = num1+num2;
+        return sum;
+    }
+
 }
