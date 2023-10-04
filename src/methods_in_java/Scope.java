f76588d4e319a4e844057a10d16269546b062d16
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-04 07:26:34 +0530

    added scope

---
+++ src/methods_in_java/Scope.java
@@ -0,0 +1,23 @@
+package methods_in_java;
+
+public class Scope {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        String name = "Kunal";
+        {
+//                int a = 78; // already initialised outside the block in the same method, hence you cannot initialise again
+            a = 100; // reassign the origin ref variable to some other value
+            System.out.println(a);
+            int c = 99;
+            name = "Rahul";
+            System.out.println(name);
+            // values initialised in this block, will remain in block
+        }
+        System.out.println(a);
+        System.out.println(name);
+    }
+}
+
