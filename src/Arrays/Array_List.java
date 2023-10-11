be71e72be60650f914d7f00ed37490c48ef61442
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-11 07:31:45 +0530

    updated Array list

--- src/Arrays/Array_List.java
+++ src/Arrays/Array_List.java
@@ -1,22 +1,38 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Array_List {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>(6);
-        list.add(72);
-        list.add(73);
-        list.add(74);
-        list.add(75);
-        list.add(76);
-        list.add(77);
-        list.add(78);
-        list.add(79);
-        list.add(80);
+//        list.add(72);
+//        list.add(73);
+//        list.add(74);
+//        list.add(75);
+//        list.add(76);
+//        list.add(77);
+//        list.add(78);
+//        list.add(79);
+//        list.add(80);
+//
+//        System.out.println(list.contains(76));
+//        System.out.println(list);
+//        list.set(0,99);
+//        list.remove(2);
+//        System.out.println(list);
 
-        System.out.println(list.contains(76));
-        System.out.println(list);
+        // input
+
+        for(int i = 0; i<6 ;i++){
+            list.add(in.nextInt());
+        }
+
+        // output
+        for(int i = 0; i<6 ;i++){
+            System.out.println(list.get(i));
+        }
 
     }
 }
