313b2c1b727a6477771bf68bcaa49800adec96a4
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-13 07:39:10 +0530

    Added linear Search

--- src/Arrays/Reversing_Array.java
+++ src/Arrays/Reversing_Array.java
@@ -1,11 +1,16 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Reversing_Array {
     public static void main(String[] args) {
-        int [] arr = {1 ,3 ,9 ,6,4};
-       reverse(arr);
+        Scanner in = new Scanner(System.in);
+        int [] arr = new int[4];
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = in.nextInt();
+        }
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void reverse(int []arr){
---
+++ src/Searching/Linear_Searching.java
@@ -0,0 +1,28 @@
+package Searching;
+
+public class Linear_Searching {
+    public static void main(String[] args) {
+        int [] arr = {2,3,66,34,99};
+        int ans = Linear_Search(arr , 2);
+        System.out.println("Element found at Index no : " +ans);
+
+    }
+
+   static int Linear_Search(int [] arr , int target) {
+        if(arr.length==0){
+            return -1;
+        }
+       for (int index = 0; index < arr.length; index++) {
+           int element = arr[index];
+           if (element==target){
+               return index;
+           }
+
+       }
+       // if element not found in Array
+       return -1;
+   }
+   }
+
+
+
