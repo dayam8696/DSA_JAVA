8e9c941c6c238455164c53ed2ede476aa5834c86
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-14 06:59:01 +0530

    Added Boolean type

--- src/Searching/Linear_Searching.java
+++ src/Searching/Linear_Searching.java
@@ -3,8 +3,10 @@ package Searching;
 public class Linear_Searching {
     public static void main(String[] args) {
         int [] arr = {2,3,66,34,99};
-        int ans = Linear_Search(arr , 2);
+        int ans = Linear_Search(arr , 34);
+        boolean ans2 = LinearSearch(arr , 34);
         System.out.println("Element found at Index no : " +ans);
+        System.out.println("Element found : " +ans2);
 
     }
 
@@ -22,6 +24,23 @@ public class Linear_Searching {
        }
        // if element not found in Array
        return -1;
+   }
+
+   // boolean
+   static boolean LinearSearch(int [] arr , int target) {
+       if(arr.length==0){
+           return false;
+       }
+       // searching element
+       for (int index = 0; index < arr.length; index++) {
+           int element = arr[index];
+           if (element==target){
+               return true;
+           }
+
+       }
+       // if element not found in Array
+       return false;
    }
    }
 
