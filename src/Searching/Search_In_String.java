fe2392a17236a931501e9f8d9cad0f8f5d0dca5b
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-14 07:11:55 +0530

    Added Boolean type

---
+++ src/Searching/Search_In_String.java
@@ -0,0 +1,24 @@
+package Searching;
+
+public class Search_In_String {
+    public static void main(String[] args) {
+        String str = "Dayam";
+        char target = 'x ';
+        System.out.println(search(str ,target));
+
+
+    }
+
+    static boolean search(String str , char target){
+        if (str.length()==0){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            int element = str.charAt(i);
+            if (element==target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
