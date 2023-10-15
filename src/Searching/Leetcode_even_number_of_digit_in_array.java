b88c32962cb8034036953ec520848a566eb05e91
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-15 07:30:45 +0530

    Added leet code questions

---
+++ src/Searching/Leetcode_even_number_of_digit_in_array.java
@@ -0,0 +1,38 @@
+package Searching;
+
+public class Leetcode_even_number_of_digit_in_array {
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(findNumber(nums));
+    }
+
+    static int findNumber(int[] nums) {
+        int count = 0;
+       for(int num :nums){
+           if (even(num)){
+           count++;}
+        }
+        return count;
+    }
+
+    // function to check that number contains even digits or not
+    static boolean even(int num) {
+        int numberOfDigits = digits(num);
+        if (numberOfDigits % 2==0){
+            return true;
+        }
+        return false;
+    }
+
+    // count number of digit in a number
+    static int digits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+}
+
+
