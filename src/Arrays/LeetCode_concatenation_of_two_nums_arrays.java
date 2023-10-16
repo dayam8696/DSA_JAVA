513cd3ee7bf65f40873e4ffa0f3733f3bb051bd6
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-16 08:03:24 +0530

    LeetCode problems

--- src/Arrays/LeetCode_concatenation_of_two_nums_arrays.java
+++ src/Arrays/LeetCode_concatenation_of_two_nums_arrays.java
@@ -1,19 +1,32 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LeetCode_concatenation_of_two_nums_arrays {
     public static void main(String[] args) {
-        int [] nums ={1,2,1};
-
+        int[] nums = {1, 2, 1};
+        int [] ans = getConcatenation(nums);
+        System.out.println(Arrays.toString(ans));
     }
+    static int[] getConcatenation(int[] nums) {
 
-//    static int [] add(int [] nums){
-//        ArrayList <Integer []> list = new ArrayList<>();
-//
-//
-//
-//    }
+        int [] nums1 = nums;
+        int [] c = new int[nums.length + nums1.length]; // size 6
+
+        for (int i = 0; i < nums.length; i++) {
+            c[i]= nums[i];
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            c[i+nums.length] = nums1[i];
+        }
+      return c;
+    }
 
 
 }
+
+
+
+
