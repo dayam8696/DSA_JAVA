d63f7f8602b50c1466554b43620a6c5ed28de47d
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-16 07:11:16 +0530

    LeetCode problems

---
+++ src/Arrays/LeetCode_Running_Sum_of_1__Array.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeetCode_Running_Sum_of_1__Array {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4};
+        int sum = runningSum(nums);
+        System.out.println(sum);
+
+    }
+    static int runningSum(int [] nums){
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+        }
+        return sum;
+    }
+
+
+
+
+}
---
+++ src/Arrays/LeetCode_concatenation_of_two_nums_arrays.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class LeetCode_concatenation_of_two_nums_arrays {
+    public static void main(String[] args) {
+        int [] nums ={1,2,1};
+
+    }
+
+//    static int [] add(int [] nums){
+//        ArrayList <Integer []> list = new ArrayList<>();
+//
+//
+//
+//    }
+
+
+}
