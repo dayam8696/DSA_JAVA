0d147f581c4045aa1617adb4d451e96bab917bf0
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-25 22:30:44 +0530

    Added binary search updated

---
+++ src/Searching/BinarySearching/Binary_Searching.java
@@ -0,0 +1,8 @@
+package Searching.BinarySearching;
+
+public class Binary_Searching {
+    public static void main(String[] args) {
+        System.out.println("Enter the sorted array ");
+    }
+
+}
