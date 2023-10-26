b12c68ef1603726ad2b9f3a8dfa67e0628520862
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-26 19:06:59 +0530

    Added arry

--- src/Searching/BinarySearching/Binary_Searching.java
+++ src/Searching/BinarySearching/Binary_Searching.java
@@ -3,6 +3,7 @@ package Searching.BinarySearching;
 public class Binary_Searching {
     public static void main(String[] args) {
         System.out.println("Enter the sorted array ");
-    }
+        int arr [] = new int[10];
+     }
 
 }
