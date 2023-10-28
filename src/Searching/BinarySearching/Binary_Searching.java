9f40cb55145c2cd4cdb81b585cfe1b9ccfb815f9
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-28 06:48:32 +0530

    BinarySearch

--- src/Searching/BinarySearching/Binary_Searching.java
+++ src/Searching/BinarySearching/Binary_Searching.java
@@ -2,8 +2,30 @@ package Searching.BinarySearching;
 
 public class Binary_Searching {
     public static void main(String[] args) {
-        System.out.println("Enter the sorted array ");
-        int arr [] = new int[10];
-     }
+        int[] arr = {2, 4, 6, 9, 11, 33, 45, 66, 78, 89, 99};
+        int target = 89;
+        int ans = BinarySearch(arr, target);
+        System.out.println(ans);
+    }
 
+    static int BinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            // find the mid
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            }
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return  -1 ;
+
+    }
 }
