b3bd6a0092976bcc18885a7d81fb63cbca36d31d
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-10-31 07:22:27 +0530

    Ceiling of the number problem

--- src/Searching/BinarySearching/Question_Ceiling_of_a_Number.java
+++ src/Searching/BinarySearching/Question_Ceiling_of_a_Number.java
@@ -1,5 +1,33 @@
 package Searching.BinarySearching;
 
 public class Question_Ceiling_of_a_Number {
+    public static void main(String[] args) {
+        int[] arr = {2, 3 , 5 ,9,14,16,18};
+        int target = 15;
+        int ans = ceiling(arr,target);
+        System.out.println(ans);
 
+    }
+
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            // find the mid
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            } else{
+                return mid;
+            }
+
+        }
+
+        return arr[start] ;
+
+    }
 }
