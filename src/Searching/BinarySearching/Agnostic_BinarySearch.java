1926475f5a13625105dea6de65dbd25153e2fe24
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-07 22:10:27 +0530

    Updated

--- src/Searching/BinarySearching/Agnostic_BinarySearch.java
+++ src/Searching/BinarySearching/Agnostic_BinarySearch.java
@@ -1,5 +1,5 @@
 package Searching.BinarySearching;
-//when user inter sorrted array of his choice
+//when user inter sorrted array of his choice or need
 public class Agnostic_BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 9, 11, 33, 45, 66, 78, 89, 99}; // acending order
