631038f6fc6af808633dbfc7dd175cd47382e5ea
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-10 07:48:58 +0530

    floor question

--- src/Searching/BinarySearching/Question_Floor_of_a_Number.java
+++ src/Searching/BinarySearching/Question_Floor_of_a_Number.java
@@ -2,6 +2,38 @@ package Searching.BinarySearching;
 
 public class Question_Floor_of_a_Number {
     public static void main(String[] args) {
+        // defining the array
+        int[] arr = {2, 3 , 5 ,9,14,16,18, 90};
+        int target = 15;
+        int ans = floor(arr,target);
+        System.out.println(ans);
+
+    }
+
+
+    static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            // find the mid
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            } else{
+                return mid;
+            }
+
+        }
+        // to return the index of that element ceiling of the target:
+        return end;
+
+        // to return the value of that element ceiling of the target:
+        // return arr[start] ;
 
     }
 }
+
