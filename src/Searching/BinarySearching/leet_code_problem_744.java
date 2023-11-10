07f3d281f4fd40bc407813178462bfcf2fb16999
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-10 10:13:56 +0530

    leet code  question

--- src/Searching/BinarySearching/leet_code_problem_744.java
+++ src/Searching/BinarySearching/leet_code_problem_744.java
@@ -4,5 +4,33 @@ package Searching.BinarySearching;
 // Nmae of the problem -  Find Smallest Letter Greater Than Target
 
 public class leet_code_problem_744 {
+    public static void main(String[] args) {
+     char [] letters = {'c' ,'f','j'};
+     char target = 'e';
+        System.out.println(nextGreatestLetter(letters , target));
+    }
 
+   static public char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            // find the mid
+            int mid = start + (end - start) / 2;
+            if (target < letters[mid]) {
+                end = mid - 1;
+
+            }
+            else  {
+                start = mid + 1;
+            }
+
+        }
+
+         return letters[start%letters.length];
+
+
+    }
 }
+
+
