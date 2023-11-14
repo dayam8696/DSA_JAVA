0e35458d86853f43a9bf1bed8cc1a89194e17f35
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-14 08:50:29 +0530

    infinite_array_problem

---
+++ src/Searching/BinarySearching/infinite_Array_problem.java
@@ -0,0 +1,41 @@
+package Searching.BinarySearching;
+
+public class infinite_Array_problem {
+    public static void main(String[] args) {
+        int [] arr = {3,5,7,9,10,90,100,130,140,160,170};
+        int target = 10;
+        System.out.println(ans(arr,target));
+
+    }
+
+    static int ans (int [] arr ,int target){
+        int start = 0;
+        int end = 1;
+
+        while (target>arr[end]){
+            int temp = end +1;
+            end = end + (end-start+1)*2;
+            start = temp;
+        }
+        return BinarySearch(arr ,target,start,end);
+    }
+
+    static int BinarySearch(int[] arr, int target , int start , int end) {
+        while (start <= end) {
+            // find the mid
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            } else{
+                return mid;
+            }
+
+        }
+        return  -1 ;
+
+    }
+}
