01ae00123394966a7cfd8e798c4bcac0f89aba90
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-11-15 09:06:46 +0530

    mountain peak_array_problem

--- src/Searching/BinarySearching/peak_in_mountain_array.java
+++ src/Searching/BinarySearching/peak_in_mountain_array.java
@@ -2,20 +2,22 @@ package Searching.BinarySearching;
 
 public class peak_in_mountain_array {
     public static void main(String[] args) {
-        int [] arr = {1 ,2,3,5,6,4,3,2};
+        int[] arr = {1, 2, 3, 5, 6, 4, 3, 2};
         System.out.println(peakIndexInMountainArray(arr));
 
     }
-   static public int peakIndexInMountainArray(int[] arr) {
+
+    static public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end = arr.length-1;
-        int mid = start + (end - start) / 2;
-        if(arr[mid]>arr[mid+1]){
-            end = mid;
+        int end = arr.length - 1;
+        while (start > end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }}
+            return start;
         }
-        else {
-            start=mid+1;
-        }
-return start;
     }
-}
+
