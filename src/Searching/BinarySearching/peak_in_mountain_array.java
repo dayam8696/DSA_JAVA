24a9c55b372cf61d55424a114be046c8732d895c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-25 07:11:52 +0530

    updated

--- src/Searching/BinarySearching/peak_in_mountain_array.java
+++ src/Searching/BinarySearching/peak_in_mountain_array.java
@@ -10,7 +10,7 @@ public class peak_in_mountain_array {
     static public int peakIndexInMountainArray(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
-        while (start > end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] > arr[mid + 1]) {
                 end = mid;
