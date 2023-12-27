1bdc107c62cc6424cb05b2c5e9d6ed0244e9a175
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-27 10:58:38 +0530

    added peak in the mountain

--- src/Searching/BinarySearching/peak_in_mountain_array.java
+++ src/Searching/BinarySearching/peak_in_mountain_array.java
@@ -2,7 +2,7 @@ package Searching.BinarySearching;
 
 public class peak_in_mountain_array {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 6, 4, 3, 2};
+        int[] arr = {1, 2, 3, 5, 6, 8, 3, 2};
         System.out.println(peakIndexInMountainArray(arr));
 
     }
