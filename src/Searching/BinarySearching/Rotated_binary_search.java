077f43f18f97906c4c0889e6f77f3a3cc700554e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2023-12-30 07:34:09 +0530

    RBS

--- src/Searching/BinarySearching/Rotated_binary_search.java
+++ src/Searching/BinarySearching/Rotated_binary_search.java
@@ -2,9 +2,31 @@ package Searching.BinarySearching;
 
 public class Rotated_binary_search {
 
+    //Leetcode problem link =https://leetcode.com/problems/search-in-rotated-sorted-array/
+
     public static void main(String[] args) {
         int [] arr = { 4,5,6,7,0,1,2};
+        int ans = findpivot(arr);
+        System.out.println(ans
+        );
+    }
 
+
+
+    static int search (int [] nums ,int target){
+        int pivot = findpivot(nums);
+
+        if(pivot ==-1){
+            return BinarySearch(nums,target,0,nums.length-1);
+        }
+        if(nums[pivot] ==target){
+            return pivot;
+        }
+
+        if(target>=nums[0]){
+            return BinarySearch(nums,target,0 ,pivot-1);
+        }
+        return BinarySearch(nums,target,pivot+1 ,nums.length-1);
     }
 
     static int findpivot(int [] arr){
@@ -13,16 +35,24 @@ public class Rotated_binary_search {
         int end = arr.length-1;
         while(start<=end){
             int mid = start + (end - start) / 2;
-            if(arr[mid]>arr[mid+1]){
+            if(mid<end && arr[mid]>arr[mid+1]){
                 return mid;
             }
 
-            if(arr[mid]<arr[mid+-1]){
+            if(mid>start && arr[mid]<arr[mid+-1]){
                 return mid-1;
             }
 
-        }
+            if(arr[mid]<=arr[start]){
+                end = mid-1;
+            }
 
+            else {
+                start = mid+1;
+            }
+
+        }
+            return -1;
     }
 
 
