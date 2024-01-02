bf594b4972d18f0f131c2067d53b03209a1fafde
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-02 07:57:13 +0530

    added sorting

--- src/Sorting/Bubble_sort.java
+++ src/Sorting/Bubble_sort.java
@@ -1,4 +1,37 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Bubble_sort {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        Bubble_sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    static void Bubble_sort(int[] arr) {
+        boolean swapped;
+        // run the step n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            //for each step ,max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+                //swap if the item is smaller than the previous item
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you did not swap for a particular value of i it means array is sorted end the program break
+            if ((!swapped)){
+                break;
+            }
+        }
+    }
 }
+
+
+
