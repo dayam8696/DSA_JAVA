2f64567a04e41b9577364432b2dd500b2b81db3e
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-03 10:07:06 +0530

    added bubble sort

--- src/Sorting/Bubble_sort.java
+++ src/Sorting/Bubble_sort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Bubble_sort {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {5,4,3,2,1};
         Bubble_sort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -16,7 +16,7 @@ public class Bubble_sort {
         for (int i = 0; i < arr.length; i++) {
             swapped = false;
             //for each step ,max item will come at the last respective index
-            for (int j = 1; j < arr.length - i; j++) {
+            for (int j = 1; j <= arr.length - i-1; j++) {
                 //swap if the item is smaller than the previous item
                 if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
