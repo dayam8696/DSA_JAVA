221d42237316f35c7363736bc0fac9a0661dfd64
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-03 10:14:32 +0530

    added slection sort

--- src/Sorting/Selection_sort.java
+++ src/Sorting/Selection_sort.java
@@ -6,5 +6,22 @@ public class Selection_sort {
 
     }
 
+    static void selection(int []arr){
+        for (int i =0 ;i<arr.length;i++){
+            int last = arr.length-i-1;
+            int maxIndex = getMaxIndex(arr ,0,last);
+            swap(a)
 
+        }
+    }
+
+     static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for(int i = start ;i<=end ;i++){
+            if(arr[max]<arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
 }
