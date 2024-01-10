6a44a36fdbbc951c3d81939c142c80793c34a437
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-01-10 09:11:38 +0530

    added cyclic sort

--- src/Sorting/Cyclic_sort.java
+++ src/Sorting/Cyclic_sort.java
@@ -1,7 +1,24 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Cyclic_sort {
     public static void main(String[] args) {
-
+        int [] arr = {3,5,2,1,4};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
     }
+    static void cyclicsort(int [] arr){
+        int i = 0;
+        while (i< arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+
 }
