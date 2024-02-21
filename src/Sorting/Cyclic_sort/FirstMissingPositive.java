12de37a71b8c096a91acd2061f3caefed3aeea82
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-21 14:22:03 +0530

    deepen into the string

--- src/Sorting/Cyclic_sort/FirstMissingPositive.java
+++ src/Sorting/Cyclic_sort/FirstMissingPositive.java
@@ -1,5 +1,40 @@
 package Sorting.Cyclic_sort;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/first-missing-positive/description/
 public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -1, 1};
+       int ans = missingPositive(arr);
+        System.out.println(ans);
+    }
 
+    public static int missingPositive(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index +1) {
+                return index + 1;
+            }
+        }
+
+
+        return arr.length + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
 }
---
+++ src/String/string.java
@@ -0,0 +1,8 @@
+package String;
+
+public class string {
+    public static void main(String[] args) {
+        String name = "Dayam Nadeem";
+        System.out.println(name);
+    }
+}
