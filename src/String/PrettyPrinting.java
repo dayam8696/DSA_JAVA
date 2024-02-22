73984a31d5c29d694710b08ba8f93c79faf795b6
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-22 11:27:06 +0530

    pretty printing

---
+++ src/String/PrettyPrinting.java
@@ -0,0 +1,8 @@
+package String;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted number is %.2f" , a);
+    }
+}
