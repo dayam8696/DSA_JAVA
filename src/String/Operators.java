4ba1fe46a7e611b89d5c98bc529646280cccbac0
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-22 11:57:34 +0530

    pretty printing

---
+++ src/String/Operators.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.ArrayList;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println((char)('a' +3));
+
+
+        System.out.println("a" +1);
+
+        System.out.println("dayam" + new ArrayList<>());
+
+    }
+}
--- src/String/PrettyPrinting.java
+++ src/String/PrettyPrinting.java
@@ -3,6 +3,7 @@ package String;
 public class PrettyPrinting {
     public static void main(String[] args) {
         float a = 453.1274f;
-        System.out.printf("Formatted number is %.2f" , a);
+    //   System.out.printf("Formatted number is %.2f" , a);
+        System.out.printf("%.2f", Math.PI);
     }
 }
