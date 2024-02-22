70ca05c17d05c9798f5b95ab71136e3a0f5a417c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-22 11:59:47 +0530

    performance

---
+++ src/String/Performance.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.sql.SQLOutput;
+
+public class Performance {
+    public static void main(String[] args) {
+        for (int i = 0 ; i<26 ; i++ ){
+            char ch = (char) ('a' +i);
+            System.out.println(ch);
+        }
+    }
+}
