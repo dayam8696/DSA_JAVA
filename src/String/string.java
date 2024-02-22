5b2df3aee8bbe847a66514a76ca85b99e4d4ab1c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-22 10:55:33 +0530

    string

--- src/String/string.java
+++ src/String/string.java
@@ -4,5 +4,10 @@ public class string {
     public static void main(String[] args) {
         String name = "Dayam Nadeem";
         System.out.println(name);
+
+        String a= "dayam";
+        String b = "dayam";
+
+        System.out.println(a.charAt(0));
     }
 }
