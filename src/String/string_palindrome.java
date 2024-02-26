7e338611b4f91d3bb3c493044ced9446dd8deb4f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-26 15:27:33 +0530

    added palindrome

--- src/String/string_palindrome.java
+++ src/String/string_palindrome.java
@@ -1,5 +1,27 @@
 package String;
 
 public class string_palindrome {
+    public static void main(String[] args) {
+        String [] dym = {"a" ,"b" ,"c","d","c","b","a"};
+        System.out.println(checkpalindrome(dym));;
+    }
 
-}
+    private static boolean checkpalindrome(String[] args) {
+        int start = 0;
+        int end = args.length-1;
+
+        while (start<end){
+            if (args[start]==args[end]){
+               return true;
+
+            }
+            start ++;
+            end--;
+            }
+
+
+
+return false;
+    }
+    }
+
