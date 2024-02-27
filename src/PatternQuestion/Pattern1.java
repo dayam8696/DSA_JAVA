11a3079d32a86f877cdafc68a998d6a75b1378a8
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-02-27 07:29:48 +0530

    added pattern1

---
+++ src/PatternQuestion/Pattern1.java
@@ -0,0 +1,12 @@
+package PatternQuestion;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        for (int row = 1; row<=4;row++){
+            for (int col = 1 ; col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
