9570fb826105c4d71c3536bb3ab489f5bbf5b51c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-01 16:57:32 +0530

    diamond done

--- src/PatternQuestion/Diamond.java
+++ src/PatternQuestion/Diamond.java
@@ -9,7 +9,8 @@ public class Diamond {
     private static void diamond(int n) {
         for (int row = 0 ; row < 2 * n ;row++){
             int colitem = row > n ? 2 * n -row :row;
-            int spitem = row > n ? row - n :n-row;
+           int spitem = row > n ? row - n :n-row;
+          //  int totalspace = n- colitem;
             for (int sp = 0; sp< spitem; sp++){
                 System.out.printf(" ");
             }
