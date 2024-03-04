2e3128a8f744d1525093c491c618fa5afcc12c9f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-04 07:21:06 +0530

    fabonacoi using  Recursion

--- src/Recursion/FabonacoiSeries.java
+++ src/Recursion/FabonacoiSeries.java
@@ -1,4 +1,15 @@
 package Recursion;
 
 public class FabonacoiSeries {
+    public static void main(String[] args) {
+        int ans = 4;
+        System.out.println(fibo(ans));
+    }
+
+    static int fibo(int n){
+        if (n <2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
 }
