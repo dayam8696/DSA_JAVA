4a6835c8c2a5e86e3db86194c59070662b95bdf1
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-05 07:26:05 +0530

    binary search using recursion

--- src/Recursion/BinarySearchUsingRecursion.java
+++ src/Recursion/BinarySearchUsingRecursion.java
@@ -4,6 +4,7 @@ public class BinarySearchUsingRecursion {
     public static void main(String[] args) {
         int arr [] = {-1 ,0,3,5,9,12};
         int target = 9;
+        System.out.println(search(arr,target,0, arr.length-1));
 
     }
 
@@ -16,5 +17,10 @@ public class BinarySearchUsingRecursion {
         if (arr[mid] == target) {
             return mid;
         }
+
+        if (target > arr[mid]){
+            return search(arr,target,mid+1,end);
+        }
+        return search(arr,target,start,mid-1);
     }
 }
