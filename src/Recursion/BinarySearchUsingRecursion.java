7616c0c12b0149ff9250526c51f1c1ea63f1f43d
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-05 07:22:09 +0530

    binary search using recursion

---
+++ src/Recursion/BinarySearchUsingRecursion.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class BinarySearchUsingRecursion {
+    public static void main(String[] args) {
+        int arr [] = {-1 ,0,3,5,9,12};
+        int target = 9;
+
+    }
+
+    static int search(int arr[] , int target ,int start,int end){
+        if (start >end){
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+    }
+}
