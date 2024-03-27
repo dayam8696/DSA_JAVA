b76211531189dd6e0642ce950123f6798a3ac24f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-03-27 06:02:41 +0530

    added fibo formula

--- src/Recursion/FabonacoiSeries.java
+++ src/Recursion/FabonacoiSeries.java
@@ -2,10 +2,14 @@ package Recursion;
 
 public class FabonacoiSeries {
     public static void main(String[] args) {
-        int ans = 4;
-        System.out.println(fibo(ans));
+        int ans = 30;
+        System.out.println(fiboformula(ans));
     }
 
+
+    static int fiboformula(int n){
+        return (int) (Math.pow(((1+ Math.sqrt(5)) / 2), n) / Math.sqrt(5));
+    }
     static int fibo(int n){
         // base condition
         if (n <2){
