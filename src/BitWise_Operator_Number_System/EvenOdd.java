6fd3f2c9492d56987f0ef482444818736052f5a6
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-04 06:08:57 +0530

    Added bitwise operator

--- src/BitWise_Operator_Number_System/EvenOdd.java
+++ src/BitWise_Operator_Number_System/EvenOdd.java
@@ -2,11 +2,11 @@ package BitWise_Operator_Number_System;
 
 public class EvenOdd {
     public static void main(String[] args) {
-        int n = 68;
+        int n = 67;
         System.out.println(isOdd(n));
     }
 
     private static boolean isOdd(int n){
-        return n & 1
+        return (n & 1)==1;
     }
 }
