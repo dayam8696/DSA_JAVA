27fd2fbf131eb80b410fc455246f8cea2091818a
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-20 05:29:27 +0530

    count digit

---
+++ src/BitWise_Operator_Number_System/NoOfDigit.java
@@ -0,0 +1,15 @@
+package BitWise_Operator_Number_System;
+
+public class NoOfDigit {
+    public static void main(String[] args) {
+        int n = 343823;
+        int base = 10;
+
+        int ans = (int)(Math.log(n) / Math.log(base)) + 1;
+
+        System.out.println(ans);
+    }
+
+
+
+}
