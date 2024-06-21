22f3d8ad0e3f1c9e194e6e4b5fd98e8660fe1398
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-21 09:17:35 +0530

    added set bit

---
+++ src/BitWise_Operator_Number_System/SetBit.java
@@ -0,0 +1,8 @@
+package BitWise_Operator_Number_System;
+
+public class SetBit {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
