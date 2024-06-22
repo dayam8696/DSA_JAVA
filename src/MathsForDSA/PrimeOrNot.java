b02364f6ec1d62fbaca5f79b06f38cb6a40871f3
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-22 16:30:22 +0530

    Maths For Dsa

--- src/MathsForDSA/PrimeOrNot.java
+++ src/MathsForDSA/PrimeOrNot.java
@@ -1,4 +1,23 @@
 package MathsForDSA;
 
 public class PrimeOrNot {
+    public static void main(String[] args) {
+        int n = 5;
+
+    }
+    static Boolean isprime(int n){
+        if (n <=1 ){
+            return false;
+        }
+
+    int c = 2;
+        while (c*c<=n){
+            if (n%c==0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+
 }
