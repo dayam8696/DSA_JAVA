c7cc5ccb114cbdc9794a8a6e39fb3a0f79fcd5e4
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-24 15:11:03 +0530

    added factor of thr number

--- src/MathsForDSA/FactorOfTheNumber.java
+++ src/MathsForDSA/FactorOfTheNumber.java
@@ -3,7 +3,7 @@ package MathsForDSA;
 public class FactorOfTheNumber {
     public static void main(String[] args) {
         int n = 20;
-        FactorOf(n);
+        FactorOf2(n);
     }
 
    static void FactorOf(int n) {
@@ -14,5 +14,21 @@ public class FactorOfTheNumber {
 
            }
        }
+    }
+
+    static void FactorOf2(int n) {
+        for (int i = 1; i <=Math.sqrt(n) ; i++) {
+            if (n%i==0) {
+                if (n/i==i){
+                    System.out.print(i + " ");
+                }
+                else {
+                    System.out.print(i + " " + n/i + " ");
+                }
+
+
+
+            }
+        }
     }
 }
