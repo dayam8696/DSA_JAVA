a950339d90f8ac1b3b966bdcba4ca2e2dced8da4
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-24 15:24:27 +0530

    added factor of thr numbersorted + optimised

--- src/MathsForDSA/FactorOfTheNumber.java
+++ src/MathsForDSA/FactorOfTheNumber.java
@@ -1,9 +1,11 @@
 package MathsForDSA;
 
+import java.util.ArrayList;
+
 public class FactorOfTheNumber {
     public static void main(String[] args) {
         int n = 20;
-        FactorOf2(n);
+        FactorOf3(n);
     }
 
    static void FactorOf(int n) {
@@ -15,7 +17,7 @@ public class FactorOfTheNumber {
            }
        }
     }
-
+// optimised
     static void FactorOf2(int n) {
         for (int i = 1; i <=Math.sqrt(n) ; i++) {
             if (n%i==0) {
@@ -29,6 +31,29 @@ public class FactorOfTheNumber {
 
 
             }
+        }
+    }
+
+//optimised + accending
+    static void FactorOf3(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <=Math.sqrt(n) ; i++) {
+            if (n%i==0) {
+                if (n/i==i){
+                    System.out.print(i + " ");
+                }
+                else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+
+
+
+            }
+        }
+        for (int i = list.size() -1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+
         }
     }
 }
