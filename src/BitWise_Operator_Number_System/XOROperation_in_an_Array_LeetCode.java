9fee42dc046933193d9a9be4131aecf5a84d751f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-25 21:47:23 +0530

    added Leetcode Question

---
+++ src/BitWise_Operator_Number_System/XOROperation_in_an_Array_LeetCode.java
@@ -0,0 +1,26 @@
+package BitWise_Operator_Number_System;
+
+public class XOROperation_in_an_Array_LeetCode {
+    public static void main(String[] args) {
+        XOROperation_in_an_Array_LeetCode solution = new XOROperation_in_an_Array_LeetCode();
+
+        // Test case 1
+        int n1 = 5;
+        int start1 = 0;
+        System.out.println("Output: " + solution.xorOperation(n1, start1)); // Output: 8
+
+        // Test case 2
+        int n2 = 4;
+        int start2 = 3;
+        System.out.println("Output: " + solution.xorOperation(n2, start2)); // Output: 8
+    }
+
+    public int xorOperation(int n, int start) {
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor ^= start + 2 * i;
+        }
+        return xor;
+    }
+
+}
