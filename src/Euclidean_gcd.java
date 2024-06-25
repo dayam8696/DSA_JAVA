1b9dfcaf32b167a5d4e9abfee3173ecb9e00b0ff
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-25 12:27:20 +0530

    added HCF

---
+++ src/Euclidean_gcd.java
@@ -0,0 +1,15 @@
+public class Euclidean_gcd {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 18;
+        System.out.println(gcd(a,b));
+    }
+
+  static int gcd(int a, int b) {
+        if (a==0){
+            return b;
+
+      }
+        return gcd(b%a,a);
+    }
+}
