b5173adea038434ff307b81cf6e8ad9520aa5c16
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-28 06:08:57 +0530

    Soorted or not using recursion done

--- src/Recursion_Arrays/Sorted_Or_Not.java
+++ src/Recursion_Arrays/Sorted_Or_Not.java
@@ -1,7 +1,18 @@
 package Recursion_Arrays;
 
+import org.jetbrains.annotations.Contract;
+
 public class Sorted_Or_Not {
     public static void main(String[] args) {
+        int arr [] = {1,2,3,4,6};
+        System.out.println(IsSorted(arr , 0));
+    }
 
+    @Contract(pure = true)
+    static boolean IsSorted(int[] arr, int i) {
+        if (i == arr.length-1){
+            return true;
+        }
+        return arr[i]<arr[i+1] && IsSorted(arr , i+1);
     }
 }
