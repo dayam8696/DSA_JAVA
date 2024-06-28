4d5c71d2bdbfd3f75882f71503515e5c7e514396
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-28 06:25:23 +0530

    added linear search via recursion exact index

--- src/Recursion_Arrays/Find_Element_Linear_Search.java
+++ src/Recursion_Arrays/Find_Element_Linear_Search.java
@@ -5,12 +5,26 @@ public class Find_Element_Linear_Search {
         int arr [] = {1,5,3,9,18,4};
         int target = 9;
         System.out.println(Find(arr , target ,0));
+        System.out.println("The element is at index number : " + FindIndex(arr,target,0));
     }
 
-    private static boolean Find(int[] arr, int target, int index) {
+
+    //Returining Boolean
+    static boolean Find(int[] arr, int target, int index) {
         if (index==arr.length){
             return false;
         }
         return arr[index]==target || Find(arr,target,index+1);
+    }
+
+    // Returining exact index value
+     static int FindIndex(int[] arr, int target, int index) {
+        if (index==arr.length){
+            return -1;
+        }
+        if (arr[index]==target){
+            return index;
+        }
+        return FindIndex(arr,target,index+1);
     }
 }
