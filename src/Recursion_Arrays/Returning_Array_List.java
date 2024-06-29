a9b7836fa99457b50bb9d835124716e604746fb9
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-30 05:27:22 +0530

    Added retutning array list

--- src/Recursion_Arrays/Returning_Array_List.java
+++ src/Recursion_Arrays/Returning_Array_List.java
@@ -1,7 +1,22 @@
 package Recursion_Arrays;
 
+import java.util.ArrayList;
+
 public class Returning_Array_List {
     public static void main(String[] args) {
+        int arr [] = {2,1,3,4,4,5};
+        ArrayList<Integer> ans = findAllIndex(arr,4,0,new ArrayList<Integer>());
+        System.out.println(ans);
+    }
 
+    private static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index==arr.length){
+            return list;
+
+        }
+        if (arr[index]==target){
+            list.add(index);
+        }
+        return findAllIndex(arr,target,index+1,list);
     }
 }
