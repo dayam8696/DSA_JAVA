752b2aa197e76898a8420a8e1c5e926e2e901d3f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-07-01 05:47:17 +0530

    Recursion_Pattern_Questiondone 1

--- src/Recursion_Pattern_Question/Triangle.java
+++ src/Recursion_Pattern_Question/Triangle.java
@@ -3,6 +3,19 @@ package Recursion_Pattern_Question;
 public class Triangle {
 
     public static void main(String[] args) {
-
+            triangle(4,0);
+    }
+    static void triangle(int r , int c){
+        if (r==0){
+            return;
+        }
+        if (c<r){
+            System.out.print("*");
+            triangle(r,c+1);
+        }
+        else {
+            System.out.println();
+            triangle(r-1,0);
+        }
     }
 }
