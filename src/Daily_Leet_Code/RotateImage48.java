bf845de52abac6fd63d72b9edcf6ab05f7d9404c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-07-04 08:46:13 +0530

    48. Rotate Image

---
+++ src/Daily_Leet_Code/RotateImage48.java
@@ -0,0 +1,45 @@
+package Daily_Leet_Code;
+
+public class RotateImage48 {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        rotate(matrix1);
+        printMatrix(matrix1);
+    }
+
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
