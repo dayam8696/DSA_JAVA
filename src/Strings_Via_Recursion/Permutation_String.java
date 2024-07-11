1c1350e44bfe9dff7ebe5ca4fabdf10e7193aa71
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-07-11 05:54:10 +0530

    permutation String

---
+++ src/Strings_Via_Recursion/Permutation_String.java
@@ -0,0 +1,19 @@
+package Strings_Via_Recursion;
+
+public class Permutation_String {
+    public static void main(String[] args) {
+    permutation("","abc");
+    }
+    static void permutation(String p ,String up){
+        if (up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        for (int i = 0; i <=p.length(); i++) {
+            String f = p.substring(0,i);
+            String s =p.substring(i,p.length());
+            permutation(f+ch+ s,up.substring(1));
+        }
+    }
+}
