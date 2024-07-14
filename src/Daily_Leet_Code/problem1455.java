56d9dbfa67bd1bb4d2871101acdf52536b423d15
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-07-14 09:01:30 +0530

    Reverse vowel of string 345

--- src/Daily_Leet_Code/problem1455.java
+++ src/Daily_Leet_Code/problem1455.java
@@ -4,7 +4,8 @@ public class problem1455 {
     public static void main(String[] args) {
         String sentence = "i love eating burger" ;
         String searchwords = "burg";
-        System.out.println(checkPrefix(sentence,searchwords));
+       // System.out.println(checkPrefix(sentence,searchwords));
+        System.out.println(reverse(sentence));
     }
     public static int checkPrefix(String sentence , String searchword){
 
@@ -15,5 +16,27 @@ public class problem1455 {
             }
         }
         return -1;
+    }
+
+    public static String reverse(String sentence ){
+
+        String [] sentences = sentence.split(" ");
+        int start = 0;
+        int  end= sentence.length()  ;
+        while (start<end){
+            swaapping(sentences,start,end);
+            start++;
+            end--;
+        }
+        return sentence = String.join(" " , sentence);
+    }
+
+
+    static void swaapping(String []arr ,int index1, int index2){
+        String temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+
+
     }
 }
