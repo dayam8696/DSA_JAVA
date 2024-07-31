f426a7f77cc04f19fb635f2e4cd643732b5a3614
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-07-31 16:12:22 +0530

    done with this question

--- src/Daily_Leet_Code/MinimumNumberofArrowstoBurstBalloons452.java
+++ src/Daily_Leet_Code/MinimumNumberofArrowstoBurstBalloons452.java
@@ -1,8 +1,26 @@
 package Daily_Leet_Code;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MinimumNumberofArrowstoBurstBalloons452 {
     public static void main(String[] args) {
-
+            int[][] points = {{10,16},{2,8},{1,6},{7,12}};
+        System.out.println(findMinArrowShots(points));
     }
 
+    public static int findMinArrowShots(int[][] points) {
+        Arrays.sort(points,(a,b)-> Integer.compare(a[1],b[1]));
+        int arrow = 1 ,end = points[0][1];
+
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0]>end){
+                arrow++;
+                end = points[i][1];
+            }
+            
+        }
+        return arrow;
+    }
 }
+
