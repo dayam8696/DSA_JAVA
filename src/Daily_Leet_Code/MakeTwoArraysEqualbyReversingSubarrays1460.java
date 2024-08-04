669f15ca0da9305e87a8a3c85d2c16425b7c9ad5
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-04 17:20:00 +0530

    added 1560

--- src/Daily_Leet_Code/MakeTwoArraysEqualbyReversingSubarrays1460.java
+++ src/Daily_Leet_Code/MakeTwoArraysEqualbyReversingSubarrays1460.java
@@ -6,6 +6,7 @@ public class MakeTwoArraysEqualbyReversingSubarrays1460 {
     public static void main(String[] args) {
         int [] target = {1,2,3,4};
             int [] arr = {2,4,1,3};
+        System.out.println();
         System.out.println(canBeEqual(target,arr));
     }
     public static boolean canBeEqual(int[] target, int[] arr) {
