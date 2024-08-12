b21b50819dfaeadbd09ab8ee4904fae80aab7cd3
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-12 11:08:32 +0530

    added two integer number question

---
+++ src/Daily_Leet_Code/AddTwoIntegers2235.java
@@ -0,0 +1,11 @@
+package Daily_Leet_Code;
+
+public class AddTwoIntegers2235 {
+    public static void main(String[] args) {
+        System.out.println(sum(-10,4));
+    }
+    public static int sum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+}
