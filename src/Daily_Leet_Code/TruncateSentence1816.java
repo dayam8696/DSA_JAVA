16ba17f9c53a0832a08329294b6408c96cbb7bba
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-13 10:12:39 +0530

    1816. Truncate Sentence

---
+++ src/Daily_Leet_Code/TruncateSentence1816.java
@@ -0,0 +1,21 @@
+package Daily_Leet_Code;
+
+public class TruncateSentence1816 {
+    public static void main(String[] args) {
+        String s = "Hello how are you Contestant";
+        int k = 4;
+        System.out.println(truncateSentence(s,k));
+    }
+    public static String truncateSentence(String s, int k){
+        int len = s.length();
+        String words[] = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        int index = 0 ;
+        while (k!=0){
+            sb.append(words[index] + " ");
+            index++;
+            k--;
+        }
+        return sb.toString();
+    }
+}
