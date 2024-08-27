4d4e324829c8108969a325474bc7bed2ae1510d2
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-27 06:45:00 +0530

    Inner Classes

---
+++ src/OOPs_Java/statice_example/InnerClasses.java
@@ -0,0 +1,24 @@
+package OOPs_Java.statice_example;
+
+public class InnerClasses {
+    static class Test {
+        String name;
+        public Test(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test("Kunal");
+        Test b = new Test("Rahul");
+
+       // System.out.println(a);
+      System.out.println(a.name);
+       System.out.println(b.name);
+    }
+}
