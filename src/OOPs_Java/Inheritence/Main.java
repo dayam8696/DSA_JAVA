53815c2459a6fe8c89a949497056d2cf06915474
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-30 15:56:13 +0530

    added inheritence

---
+++ src/OOPs_Java/Inheritence/Main.java
@@ -0,0 +1,8 @@
+package OOPs_Java.Inheritence;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4,4,5);
+        System.out.println(box.h + " " + box.w + " " + box.l);
+    }
+}
