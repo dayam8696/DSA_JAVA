804b59a6276dd9d36fc02a3dda2b90e44337bb10
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-08-30 08:50:08 +0530

    added singleton class

---
+++ src/OOPs_Java/Singleton/main.java
@@ -0,0 +1,18 @@
+package OOPs_Java.Singleton;
+
+
+
+public class main {
+    public static void main(String[] args) {
+    Singleton obj1 = Singleton.getInstance();
+
+    Singleton obj2 = Singleton.getInstance();
+
+    Singleton obj3 = Singleton.getInstance();
+
+    // all 3 ref variables are pointing to just one object
+
+
+
+}
+}
