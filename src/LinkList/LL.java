9d4a0ff4202d48fa747a97140249159295ab2ed7
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-09-15 15:46:10 +0530

    Inserted in between

--- src/LinkList/LL.java
+++ src/LinkList/LL.java
@@ -30,6 +30,23 @@ public void insertLast(int val ){
 
 
 }
+public void insertInBetween(int val , int index){
+        if (index==0){
+            insertFirst(val);
+            return;
+        }
+        if(index==size){
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+    for (int i = 1; i <index ; i++) {
+        temp = temp.next;
+    }
+    Node node = new Node(val,temp.next);
+    temp.next= node;
+    size++;
+}
 public void display(){
         Node temp = head;
         while (temp!= null){
--- src/LinkList/Main.java
+++ src/LinkList/Main.java
@@ -3,25 +3,26 @@ package LinkList;
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(4);
-//        list.insertFirst(5);
-//        list.insertFirst(6);
-//        list.insertFirst(7);
-//        list.insertFirst(8);
-//        list.insertFirst(9);
-//
-//
-//        list.display();
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(6);
+        list.insertFirst(7);
+        list.insertFirst(8);
+        list.insertFirst(9);
+        list.insertInBetween(20,3);
 
-        list.insertLast(3);
-        list.insertLast(4);
-        list.insertLast(5);
-        list.insertLast(6);
-        list.insertLast(7);
-        list.insertLast(8);
-        list.insertLast(9);
+
         list.display();
+
+//        list.insertLast(3);
+//        list.insertLast(4);
+//        list.insertLast(5);
+//        list.insertLast(6);
+//        list.insertLast(7);
+//        list.insertLast(8);
+//        list.insertLast(9);
+//        list.display();
 
 
     }
