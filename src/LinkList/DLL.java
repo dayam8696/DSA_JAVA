2c709b559147092053bc631269b882744458f12a
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-09-17 07:06:18 +0530

    added doubly LinkList

---
+++ src/LinkList/DLL.java
@@ -0,0 +1,44 @@
+package LinkList;
+
+public class DLL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        node.prev = null;
+        if (head!=null){
+            head.prev = node;
+
+        }
+        head = node;
+
+    }
+
+    public void display(){
+        Node node = head;
+        while (node!=null){
+            System.out.printf(node.val + " ->");
+            node=node.next;
+        }
+        System.out.println("END");
+    }
+   private class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node next, Node prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
--- src/LinkList/Main.java
+++ src/LinkList/Main.java
@@ -2,22 +2,32 @@ package LinkList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(4);
-        list.insertFirst(5);
-        list.insertFirst(6);
-        list.insertFirst(7);
-        list.insertFirst(8);
-        list.insertFirst(9);
-        list.insertInBetween(20,3);
-        list.display();
-//        list.deleteFirst();
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insertFirst(5);
+//        list.insertFirst(6);
+//        list.insertFirst(7);
+//        list.insertFirst(8);
+//        list.insertFirst(9);
+//        list.insertInBetween(20,3);
 //        list.display();
-//        list.deleteLast();
+////        list.deleteFirst();
+////        list.display();
+////        list.deleteLast();
+////        list.display();
+//        list.deleteInBetween(3);
 //        list.display();
-        list.deleteInBetween(3);
-        list.display();
+
+        DLL dlist = new DLL();
+        dlist.insertFirst(10);
+        dlist.insertFirst(20);
+        dlist.insertFirst(30);
+        dlist.insertFirst(40);
+        dlist.insertFirst(50);
+        dlist.insertFirst(60);
+        dlist.insertFirst(70);
+        dlist.display();
 
 
 
