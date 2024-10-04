121d30433666dbb0412080df0b846f7b328b308c
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-10-04 11:20:19 +0530

    added interview question

---
+++ src/LinkList/InterviewQuestions.java
@@ -0,0 +1,23 @@
+package LinkList;
+
+public class InterviewQuestions {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+        if
+    }
+}
