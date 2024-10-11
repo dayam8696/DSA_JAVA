0ed08f7e1ba17cc3cd04db8b36bb329ac24b775a
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-10-11 11:30:41 +0530

    Started Stack Data Structure

---
+++ src/Stack/In_BuiltExamplle.java
@@ -0,0 +1,19 @@
+package Stack;
+
+import java.util.Stack;
+
+public class In_BuiltExamplle {
+    public static void main(String[] args) {
+        Stack<Integer> stack  = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(4);
+        stack.push(54);
+        stack.push(30);
+        stack.push(43);
+        stack.push(99);
+
+
+        System.out.println(stack);
+    }
+}
