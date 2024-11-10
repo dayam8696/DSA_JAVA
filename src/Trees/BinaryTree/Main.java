1dbaa12c7a3a986aa5a4c4c9fb691ba4f6afc419
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-11-10 11:15:25 +0530

    done with bst

--- src/Trees/BinaryTree/Main.java
+++ src/Trees/BinaryTree/Main.java
@@ -1,12 +1,18 @@
 package Trees.BinaryTree;
 
+import Trees.BST;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        BinaryTree tree = new BinaryTree();
-        tree.populate(scanner);
-        tree.prettyDisplay();
+//        Scanner scanner =new Scanner(System.in);
+//        BinaryTree tree = new BinaryTree();
+//        tree.populate(scanner);
+//        tree.prettyDisplay();
+        BST tree = new BST();
+        int [] nums = {5,4,1,8,9,2,7,10,6,3};
+        tree.populate(nums);
+        tree.display();
       }
 }
