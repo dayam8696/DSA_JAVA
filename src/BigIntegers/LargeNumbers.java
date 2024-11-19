93576fac77108cc7aa3c71c44e1a24f265d479a7
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-11-19 18:12:43 +0530

    BigInteger

---
+++ src/BigIntegers/LargeNumbers.java
@@ -0,0 +1,13 @@
+package BigIntegers;
+
+import java.math.BigInteger;
+
+public class LargeNumbers {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 67;
+        BigInteger A= BigInteger.valueOf(33);
+        BigInteger B = BigInteger.valueOf(123456789);
+
+    }
+}
--- src/StringBuffer/Main.java
+++ src/StringBuffer/Main.java
@@ -1,5 +1,9 @@
 package StringBuffer;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer();
@@ -14,6 +18,26 @@ public class Main {
 
       String str = sb.toString();
         System.out.println(str);
+
+        int size = 20;
+        String name = RandomString.generate(size);
+        System.out.println(name);
+
+        // remove whiteSpaces
+
+        String sentence = "Hi h hija jdfjsdj  dfnklsa    ew";
+        System.out.println(sentence);
+        System.out.println(sentence.replaceAll("\\s",""));
+
+        // Split
+
+        String arr = "kunal, Apoorv, Rahul, Snehal";
+        String []names = arr.split("  ");
+        System.out.println(Arrays.toString(names));
+
+        //Round OFF
+        DecimalFormat df = new DecimalFormat("0.000");
+        System.out.println(df.format(7.29));
 
 
     }
