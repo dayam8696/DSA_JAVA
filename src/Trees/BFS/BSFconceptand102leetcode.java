6c084dbff006295d30d99a1cce73b15ffc4553e1
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-12-29 21:48:17 +0530

    done zigzag order traversal

--- src/Trees/BFS/BSFconceptand102leetcode.java
+++ src/Trees/BFS/BSFconceptand102leetcode.java
@@ -1,10 +1,7 @@
 package Trees.BFS;
 
 import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 //  Definition for a binary tree node.
 
@@ -79,5 +76,52 @@ public class BSFconceptand102leetcode {
         }
         return cool;
 
+    }
+
+
+    //103. Binary Tree Zigzag Level Order Traversal
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+                if(!reverse){
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.addLast(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.addLast(currentNode.right);
+                    }
+                }
+                else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+
+                }
+
+            }
+            reverse =!reverse;
+
+            result.add(currentLevel);
+        }
+        return result;
     }
 }
